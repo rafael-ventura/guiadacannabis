377cda963bf417c0e6a94cc684e55f495e4485dd
rafael-ventura <28628701+rafael-ventura@users.noreply.github.com>
2025-09-17 23:53:46 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Guia do cultivo canábico.</h1>
+    <v-btn flat to="/welcome" nuxt> Entre para o Guia </v-btn>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  }
+};
+</script>
